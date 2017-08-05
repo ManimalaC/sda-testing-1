5ce206b1a9714c453cba433bcebd75c44828efa0
Simon Larsen <slarse@kth.se>
2017-08-05 20:47:40 +0200

    Add javadocs to Sieve

--- src/Sieve.java
+++ src/Sieve.java
@@ -1,6 +1,22 @@
 import java.util.Arrays;
+/**
+ * Implementation of the Sieve of Eratosthenes algorithm for checking if a
+ * number is prime or not. The implementation is lacking in error-checking
+ * and optimization, and needs some patching up!
+ *
+ * @author Simon Larsén
+ * @version 2017-08-05
+ */
 public class Sieve {
 
+    /**
+     * Check if a number is prime or not!
+     *
+     * Note that prime[n] denotes the primality of number n.
+     *
+     * @param   number  An integer value to be checked for primality.
+     * @return  true if number is prime, false otherwise.
+     */
     public boolean isPrime(int number) {
         boolean[] prime = new boolean[number + 1]; // + 1 because of 0-indexing
         Arrays.fill(prime, true); // assume all numbers are prime
