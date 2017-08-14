a866d8df6a1085b759a03e5ef84f5737f759f754
Simon Larsén <slarse@kth.se>
2017-08-14 13:49:59 +0200

    Make flooring in int casting explicit, fix #2

--- src/Sieve.java
+++ src/Sieve.java
@@ -20,7 +20,7 @@ public class Sieve {
     public boolean isPrime(int number) {
         boolean[] prime = new boolean[number + 1]; // + 1 because of 0-indexing
         Arrays.fill(prime, true); // assume all numbers are prime
-        int sqrt = (int) Math.sqrt(number);
+        int sqrt = (int) Math.floor(Math.sqrt(number));
         for (int i = 2; i <= sqrt; i++) {
             if (prime[i]) {
                 for (int j = i*2; j < prime.length; j+=i) {
